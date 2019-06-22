058abe95aaa00ec67ded9f27055f2658ef613608
DaigoMiyoshi <daigomiyoshi@DaigoMiyoshi-no-MacBook-Pro.local>
2019-06-23 02:41:10 +0900

    mod. scraping jupyter code

--- Config/params_config.py
+++ Config/params_config.py
@@ -1,11 +1,7 @@
-
 parameters = {
-
     # parameters for scraping
-
     'TARGET_URL': 'https://race.netkeiba.com/?pid=schedule&select=schedule&year={YEAR}',
     'YEAR_RANGE': range(2002, 2019+1),
-    'DRIVER_MAC': "./chromedriver",
+    'DRIVER_DIR': "./chromedriver",
     'PAGE_LOAD_TIMEOUT': 10
-    # 'INTERVAL': 3
 }
